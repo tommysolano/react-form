793702316a0c2ebbcc744c05b8ba8c24f103df2f
tommy <tommysolano18@hotmail.com>
2021-10-28 21:14:13 -0500

    select

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 const App = () => {
 
-  const [value, setValue] = useState({normal: "", texto: ""})
+  const [value, setValue] = useState({normal: "", texto: "", select: ""})
   const handleChange = (e) => {
     setValue({
       ...value,
@@ -15,7 +15,18 @@ const App = () => {
       {value.length < 5 ? <span>Longitud menor a 5</span> : null}
       <input type="text" name="normal" value={value.normal} onChange={handleChange}/>
       <textarea name="texto" onChange={handleChange} value={value.texto}/>
+
+
+      <select value={value.select} name="select" onChange={handleChange}>
+        <option value="">-- Seleccione --</option>
+        <option value="chanchofeliz">chancho feliz</option>
+        <option value="chanchitofeliz">chanchito feliz</option>
+        <option value="chanchitotriste">chanchito triste</option>
+        <option value="felipe">felipe</option>
+      </select>
     </div>
+
+
   )
 }
 
